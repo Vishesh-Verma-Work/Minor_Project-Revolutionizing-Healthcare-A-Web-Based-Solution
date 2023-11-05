67371b4eb70392567bf291ba162d37ed70a86ad9
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2023-11-05 02:48:01 -0800

    Added JS file + Functional NavBar

---
+++ app.js
@@ -0,0 +1,20 @@
+    // Header
+    
+let hamburger = document.querySelector("#hamburger");
+let headerDown  = document.querySelector(".header-down");
+let cross  = document.querySelector("#cross");
+
+hamburger.addEventListener("click", ()=> {
+    headerDown.style.display = "flex";
+    hamburger.style.display = "none";
+    cross.style.display = "flex";
+    console.log("Clicked");
+});
+
+cross.addEventListener("click", ()=> {
+    headerDown.style.display = "none";
+    hamburger.style.display = "flex";
+    cross.style.display = "none";
+});
+
+
