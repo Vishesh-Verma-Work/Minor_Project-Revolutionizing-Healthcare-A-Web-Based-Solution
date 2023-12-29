21500b402b67a65a008f3ce5cb07e12c67b0a0b5
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2023-12-29 07:14:28 -0800

    completed backed end upto slot route

--- index.js
+++ index.js
@@ -99,10 +99,11 @@ app.post("/check", async (req, res) => {
 
 app.post("/search/:id/book", async (req, res) => {
     let {id} = req.params;
-    let data = await Hospitals.findOne({ _id : id  });
+    let data = await Hospitals.findOne({ _id : id ,  });
     console.log(`ID : ${id} is selected`);
     console.log(`All data : ${data}`);
-    res.render("bookSlot.ejs", {data, letter : letter = ["a","b","c","d","e","f","g"], num : num = [1,2] ,id });
+
+    res.render("bookSlot.ejs", {data, letter : letter = ["a1","a2", "b1", "b2", "c1","c2","d1","d2","e1","e2","f1","f2","g1","g2"],id });
 });
 
 
